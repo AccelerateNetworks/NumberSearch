3e025b6486efdbecb3192b913c042c7e4ad6c010
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-01-14 22:02:31 -0800

    Added a parallel implementation of the TeleAPI ingest process.

--- NumberSearch.Ingest/TeleMessage.cs
+++ NumberSearch.Ingest/TeleMessage.cs
@@ -1,8 +1,10 @@
 ﻿using NumberSearch.DataAccess;
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NumberSearch.Ingest
@@ -17,7 +19,7 @@ namespace NumberSearch.Ingest
         /// <returns></returns>
         public static async Task<IngestStatistics> IngestPhoneNumbersAsync(Guid token, string connectionString)
         {
-            var readyToSubmit = new List<PhoneNumber>();
+            var readyToSubmit = new ConcurrentDictionary<string, PhoneNumber>();
 
             var start = DateTime.Now;
 
@@ -30,14 +32,55 @@ namespace NumberSearch.Ingest
                 var nxxs = await GetValidNXXsAsync(npa, token);
 
                 Console.WriteLine($"Found {nxxs.Length} NXXs");
-                foreach (var nxx in nxxs)
+
+                if (nxxs.Length > 1)
                 {
-                    readyToSubmit.AddRange(await GetValidXXXXsAsync(npa, nxx, token));
-                    Console.WriteLine($"Found {readyToSubmit.Count} Phone Numbers");
+                    // Execute these API requests in parallel.
+                    var semaphore = new SemaphoreSlim(Environment.ProcessorCount);
+                    var results = new List<Task<int>>();
+
+                    foreach (var nxx in nxxs)
+                    {
+                        // Wait for an open slot in the semaphore before grabbing another thread from the threadpool.
+                        await semaphore.WaitAsync();
+                        results.Add(Task.Run(async () =>
+                        {
+                            try
+                            {
+                                var localNumbers = await GetValidXXXXsAsync(npa, nxx, token);
+                                foreach (var num in localNumbers)
+                                {
+                                    // TODO: Maybe do something with this check varible?
+                                    var check = readyToSubmit.TryAdd(num.DialedNumber, num);
+                                }
+                                return localNumbers.Length;
+                            }
+                            finally
+                            {
+                                semaphore.Release();
+                            }
+                        }));
+                    }
+                    var complete = await Task.WhenAll(results);
+
+                    // Total the numbers retrived.
+                    int count = 0;
+                    foreach (var xxxx in complete)
+                    {
+                        count += xxxx;
+                    }
+                    Console.WriteLine($"Found {count} Phone Numbers");
                 }
             }
 
-            var stats = await Program.SubmitPhoneNumbersAsync(readyToSubmit.ToArray(), connectionString);
+            // Pull just the objects out of the concurrent data structure.
+            var numbersReady = new List<PhoneNumber>();
+            foreach (var number in readyToSubmit)
+            {
+                numbersReady.Add(number.Value);
+            }
+
+            var stats = await Program.SubmitPhoneNumbersAsync(numbersReady.ToArray(), connectionString);
 
             var end = DateTime.Now;
             stats.StartDate = start;
