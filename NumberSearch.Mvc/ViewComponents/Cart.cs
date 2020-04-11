a21d8749db9ac2f3582c4de176417954b8feda58
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-04-11 11:41:00 -0700

    Got the shopping cart status working on all pages.

---
+++ NumberSearch.Mvc/ViewComponents/Cart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NumberSearch.DataAccess;
+
+namespace NumberSearch.Mvc.ViewComponents
+{
+    public class Cart : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var session = HttpContext.Session;
+            if (session.TryGetValue("Cart", out var cookie))
+            {
+                var entries = Encoding.ASCII.GetString(cookie);
+
+                // TODO: Replace the use of Newtonsoft.Json here with System.Text.Json for better performance.
+                var items = JsonConvert.DeserializeObject<List<ProductOrder>>(entries);
+
+                return View(items);
+            }
+            else
+            {
+                return View(new List<ProductOrder> { });
+            }
+        }
+    }
+}
