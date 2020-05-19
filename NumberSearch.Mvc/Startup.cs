c3f00d7f81ba6b3ad9b6a2319af5b41dd847fc04
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-05-19 12:22:18 -0700

    Cleaned up the startup file.

--- NumberSearch.Mvc/Startup.cs
+++ NumberSearch.Mvc/Startup.cs
@@ -1,10 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace NumberSearch.Mvc
 {
