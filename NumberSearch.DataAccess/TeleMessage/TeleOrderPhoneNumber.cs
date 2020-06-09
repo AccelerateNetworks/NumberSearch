26842e4af3233c6b0c1313647229832692b17c55
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-06-09 08:27:58 -0700

    Modeled the Tele endpoint for purchasing a phone number.

---
+++ NumberSearch.DataAccess/TeleMessage/TeleOrderPhoneNumber.cs
@@ -0,0 +1,54 @@
+﻿using Flurl.Http;
+
+using System;
+using System.Threading.Tasks;
+
+namespace NumberSearch.DataAccess
+{
+    /// <summary>
+    /// Models https://apidocs.teleapi.net/api/order-phone-numbers/order-single-number
+    /// </summary>
+    public class TeleOrderPhoneNumber
+    {
+        public int code { get; set; }
+        public string status { get; set; }
+        public TeleOrderResponse[] data { get; set; }
+
+        /// <summary>
+        /// Submit an order to purchase an available phone number.
+        /// </summary>
+        /// <param name="dialedNumber"></param>
+        /// <param name="callFlow"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static async Task<TeleOrderPhoneNumber> GetAsync(string dialedNumber, int callFlow, Guid token)
+        {
+            string baseUrl = "https://apiv1.teleapi.net/";
+            string endpoint = "dids/order";
+            string tokenParameter = $"?token={token}";
+            string numberParameter = $"&number={dialedNumber}";
+            string callFlowParameter = $"&call_flow={callFlow}";
+            string route = $"{baseUrl}{endpoint}{tokenParameter}{numberParameter}{callFlowParameter}";
+            return await route.GetJsonAsync<TeleOrderPhoneNumber>().ConfigureAwait(false);
+        }
+    }
+
+    public class TeleOrderResponse
+    {
+        public string id { get; set; }
+        public string user_id { get; set; }
+        public string call_flow_id { get; set; }
+        public string channel_group_id { get; set; }
+        public string voicemail_inbox_id { get; set; }
+        public string number { get; set; }
+        public string country_code { get; set; }
+        public string npa { get; set; }
+        public string nxx { get; set; }
+        public string xxxx { get; set; }
+        public string number_type { get; set; }
+        public string state { get; set; }
+        public string ratecenter { get; set; }
+        public string sms_enabled { get; set; }
+        public string cnam { get; set; }
+    }
+}
