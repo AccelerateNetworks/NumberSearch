9644218790f294e34439d0625b90e7676258acdb
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-06-29 14:17:53 -0700

    Built out the Numbers page in the Ops project.

---
+++ NumberSearch.Ops/Models/SearchResults.cs
@@ -0,0 +1,18 @@
+﻿using NumberSearch.DataAccess;
+
+using System.Collections.Generic;
+
+namespace NumberSearch.Ops
+{
+    public class SearchResults
+    {
+        public string Query { get; set; }
+        public string CleanQuery { get; set; }
+        public int NumberOfResults { get; set; }
+        public int Page { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "<Pending>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "I like arrays")]
+        public PhoneNumber[] PhoneNumbers { get; set; }
+    }
+}
