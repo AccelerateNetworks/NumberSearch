11816db108de7d0286586e5b881054a7cbe51cf8
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-08-15 13:07:33 -0700

    Added Datatables JS to the Ops app.

--- NumberSearch.Ops/wwwroot/js/site.js
+++ NumberSearch.Ops/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#table').DataTable({
+        "order": [[0, "desc"]]
+    });
+});
+
