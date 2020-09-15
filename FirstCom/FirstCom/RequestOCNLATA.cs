7537a5a58174f967ea719ccf4520d1e5bb48da58
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-09-14 23:15:15 -0700

    Fixed the Jquery deployment.

---
+++ FirstCom/FirstCom/RequestOCNLATA.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference;
+
+using System;
+using System.Threading.Tasks;
+
+namespace FirstCom
+{
+    public class RequestOCNLATA
+    {
+        public static async Task<RequestOCNLATAResponse> GetAsync(string dialedNumber, string username, string password)
+        {
+            var Auth = new Credentials
+            {
+                Username = username,
+                Password = password
+            };
+
+            using var client = new OCNLATAdipSoapClient(OCNLATAdipSoapClient.EndpointConfiguration.OCNLATAdipSoap12);
+
+            return await client.RequestOCNLATAAsync(username, password, dialedNumber, DateTime.Now.ToShortDateString()).ConfigureAwait(false);
+        }
+    }
+}
