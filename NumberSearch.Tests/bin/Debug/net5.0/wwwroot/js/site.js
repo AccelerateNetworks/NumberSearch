03e58f47cebdfbbfd4beb411e230df8f8e6ab96c
Thomas Ryan <uncheckederror@users.noreply.github.com>
2020-11-10 19:39:52 -0800

    Updated the project to dotnet 5.

---
+++ NumberSearch.Tests/bin/Debug/net5.0/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function displayBusyIndicator() {
+    document.getElementById("loading").style.display = "block";
+}
+
+$(window).on('beforeunload', function () {
+    displayBusyIndicator();
+});
+
+$(document).on('submit', 'form', function () {
+    displayBusyIndicator();
+});
+
+$('input[type="file"]').change(function (e) {
+    var fileName = e.target.files[0].name;
+    $('.custom-file-label').html(fileName);
+});
