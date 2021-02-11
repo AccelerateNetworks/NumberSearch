084b0f6b126d892a20c78d3763ba81ff25cf7ac4
Thomas Ryan <uncheckederror@users.noreply.github.com>
2021-02-10 23:55:54 -0800

    Addressed the feedback on the unassigned phone number purchasing process in #60.

--- NumberSearch.Mvc/WorkerServices/MonitorLoop.cs
+++ NumberSearch.Mvc/WorkerServices/MonitorLoop.cs
@@ -183,9 +183,13 @@ namespace NumberSearch.Mvc
                                                         productOrder.OrderResponse = JsonSerializer.Serialize(ownedNumber);
                                                         productOrder.OrderResponse = "We already own this number.";
                                                         productOrder.Completed = true;
+                                                        ownedNumber.BillingClientId = order.BillingClientId;
+                                                        ownedNumber.Notes = $"Purchased in Order {order.OrderId}";
+                                                        ownedNumber.OwnedBy = string.IsNullOrWhiteSpace(order.BusinessName) ? $"{order.FirstName} {order.LastName}" : order.BusinessName;
 
                                                         var checkVerifyOrder = await productOrder.PutAsync(_postgresql).ConfigureAwait(false);
                                                         var checkMarkPurchased = await nto.PutAsync(_postgresql).ConfigureAwait(false);
+                                                        var checkOwnedNumber = await ownedNumber.PutAsync(_postgresql).ConfigureAwait(false);
 
                                                         Log.Information($"[Background Worker] Purchased number {nto.DialedNumber} from OwnedNumbers.");
                                                     }
