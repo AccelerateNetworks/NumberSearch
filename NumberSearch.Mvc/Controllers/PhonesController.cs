07f217c153b5ebb3494ce8d11649c3dc97923d55
Thomas Ryan <uncheckederror@users.noreply.github.com>
2021-02-19 15:51:27 -0800

    Created a page for the GRP2615 videos.

---
+++ NumberSearch.Mvc/Controllers/PhonesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberSearch.Mvc.Controllers
+{
+    public class PhonesController : Controller
+    {
+
+        [HttpGet]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult GRP2615()
+        {
+            return View();
+        }
+    }
+}
