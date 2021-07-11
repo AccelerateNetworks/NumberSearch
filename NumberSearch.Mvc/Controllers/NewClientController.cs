79689713ca8d2879213ca8ca130f8dd6bec231b3
Thomas Ryan <uncheckederror@users.noreply.github.com>
2021-07-10 23:58:13 -0700

    Began work on #195.

---
+++ NumberSearch.Mvc/Controllers/NewClientController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NumberSearch.DataAccess;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumberSearch.Mvc.Controllers
+{
+    public class NewClientController : Controller
+    {
+        [HttpGet("Cart/Order/{orderId}")]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30, Location = ResponseCacheLocation.Any)]
+        public IActionResult Index(Guid orderId)
+        {
+            return View();
+        }
+
+
+        [HttpPost("Cart/Order/{orderId}/NewClient")]
+        [ValidateAntiForgeryToken]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult SubmitNewClientAsync(Guid orderId, NewClient newClient)
+        {
+            return View();
+        }
+    }
+}
