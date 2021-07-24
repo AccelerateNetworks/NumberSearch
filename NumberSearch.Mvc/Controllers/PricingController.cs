1973c526899be19309cbc7444be8319f235a4f00
Andrew S <andrew@andrewstiefel.com>
2021-07-24 15:12:20 -0700

    Create pricing page

---
+++ NumberSearch.Mvc/Controllers/PricingController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace NumberSearch.Mvc.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class PricingController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30, Location = ResponseCacheLocation.Any)]
+        public async Task<IActionResult> IndexAsync()
+        {
+            await HttpContext.Session.LoadAsync().ConfigureAwait(false);
+            var cart = Cart.GetFromSession(HttpContext.Session);
+
+            return View("Index", cart);
+        }
+    }
+}
