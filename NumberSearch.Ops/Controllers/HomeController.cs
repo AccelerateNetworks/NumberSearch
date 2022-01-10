ec985311293971eb61316ecaafa07501c3a3844a
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-01-10 13:46:47 -0800

    Added a friendly message to successfully submitted port requests in support of #257.

--- NumberSearch.Ops/Controllers/HomeController.cs
+++ NumberSearch.Ops/Controllers/HomeController.cs
@@ -3160,7 +3160,10 @@ public class HomeController : Controller
 
                             await note.PostAsync(bulkResponse?.OrderId, _bulkVSusername, _bulkVSpassword);
 
-                            responseMessages.Add($"{bulkResponse.Description} - {bulkResponse.Code}");
+                            if (!string.IsNullOrWhiteSpace(bulkResponse.Description))
+                            {
+                                responseMessages.Add($"{bulkResponse.Description} - {bulkResponse.Code}");
+                            }
                         }
                         else
                         {
@@ -3205,7 +3208,8 @@ public class HomeController : Controller
                 Order = order,
                 PortRequest = portRequest,
                 PhoneNumbers = numbers,
-                Message = string.Join(", ", responseMessages.ToArray())
+                AlertType = "alert-success",
+                Message = responseMessages.Any() ? string.Join(", ", responseMessages.ToArray()) : "🥰 Port Request was submitted to our vendors!"
             });
         }
     }
