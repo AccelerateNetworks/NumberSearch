f340234fa10eddc13b07e570b7a801e118638239
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-03-11 12:42:55 -0800

    Fixed a nullable type issue with the product items page.

--- NumberSearch.Ops/PostgreSQL/ProductItem.cs
+++ NumberSearch.Ops/PostgreSQL/ProductItem.cs
@@ -8,11 +8,11 @@ namespace AccelerateNetworks.Operations
         public Guid ProductId { get; set; }
         public Guid? ProductShipmentId { get; set; }
         public Guid? OrderId { get; set; }
-        public string SerialNumber { get; set; } = null!;
-        public string MACAddress { get; set; } = null!;
-        public string Condition { get; set; } = null!;
-        public string ExternalOrderId { get; set; } = null!;
-        public string ShipmentTrackingLink { get; set; } = null!;
+        public string? SerialNumber { get; set; } = null!;
+        public string? MACAddress { get; set; } = null!;
+        public string? Condition { get; set; } = null!;
+        public string? ExternalOrderId { get; set; } = null!;
+        public string? ShipmentTrackingLink { get; set; } = null!;
         public DateTime DateCreated { get; set; }
         public DateTime DateUpdated { get; set; }
     }
