75019654cf5a75606708340faaceff2a5828bee8
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-05-05 23:07:28 -0700

    Began work on CallWithUs integration for LrnLookups in Canada in support of #282.

---
+++ NumberSearch.DataAccess/CallWithUs/LRNLookup.cs
@@ -0,0 +1,35 @@
+﻿using Flurl.Http;
+
+using Serilog;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSearch.DataAccess.CallWithUs
+{
+    public class LRNLookup
+    {
+
+        public static async Task<LRNLookup> GetAsync(string dialedNumber, string apiKey)
+        {
+            string baseUrl = "http://lrn.callwithus.com/api/lrn/index.php";
+            string apikeyParameter = $"?key={apiKey}";
+            string eiParameter = $"&ei";
+            string didParameter = $"&number={dialedNumber}";
+            string route = $"{baseUrl}{apikeyParameter}{eiParameter}{didParameter}";
+
+            try
+            {
+                return await route.GetJsonAsync<LRNLookup>().ConfigureAwait(false);
+            }
+            catch (FlurlHttpException ex)
+            {
+                Log.Warning(await ex.GetResponseStringAsync());
+                return null;
+            }
+        }
+    }
+}
