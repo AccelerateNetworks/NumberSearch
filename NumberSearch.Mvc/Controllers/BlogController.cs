acbdf1b14b9acb22e49d1d9f39383ea552bbe77c
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-05-30 14:39:40 -0700

    Partially implemented support for fronting Write.as blog posts in support of #283.

---
+++ NumberSearch.Mvc/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+using WriteAs.NET;
+
+namespace NumberSearch.Mvc.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class BlogController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30, Location = ResponseCacheLocation.Any)]
+        public async Task<IActionResult> IndexAsync(string postName)
+        {
+            var client = new WriteAsClient("https://write.as/");
+            var allPosts = await client.GetAllPosts("acceleratenetworks");
+            if (string.IsNullOrWhiteSpace(postName))
+            {
+                return View(allPosts);
+            }
+            else
+            {
+                var post = allPosts.FirstOrDefault(x => x.Title.Contains(postName));
+                return post is not null ? View(post) : View(allPosts);
+            }
+        }
+    }
+}
