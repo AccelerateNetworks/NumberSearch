d2e6ef3ff20f1d488daed31c2c691739b7b68ecd
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-06-04 19:29:51 -0700

    Got an initial version of the blog posts working in support of #238.

--- NumberSearch.Mvc/Controllers/BlogController.cs
+++ NumberSearch.Mvc/Controllers/BlogController.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using Npgsql;
+
+using NumberSearch.DataAccess;
+
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using WriteAs.NET;
@@ -10,20 +19,54 @@ namespace NumberSearch.Mvc.Controllers
     [ApiExplorerSettings(IgnoreApi = true)]
     public class BlogController : Controller
     {
+        private readonly IConfiguration configuration;
+        private readonly string _postgresql;
+
+        public BlogController(IConfiguration config)
+        {
+            configuration = config;
+            _postgresql = configuration.GetConnectionString("PostgresqlProd");
+        }
+
         [HttpGet]
         [ResponseCache(VaryByHeader = "User-Agent", Duration = 30, Location = ResponseCacheLocation.Any)]
-        public async Task<IActionResult> IndexAsync(string postName)
+        public async Task<IActionResult> IndexAsync(string query)
         {
-            var client = new WriteAsClient("https://write.as/");
-            var allPosts = await client.GetAllPosts("acceleratenetworks");
-            if (string.IsNullOrWhiteSpace(postName))
+            using var connection = new NpgsqlConnection(_postgresql);
+
+            var posts = await connection
+                .QueryAsync<WriteAs.NET.Client.Models.Post>("SELECT \"Id\", \"Slug\", \"Appearance\", \"Language\", \"Rtl\", \"CreateDate\", \"LastUpdatedDate\", \"Title\", \"Body\", \"Views\" " +
+                "FROM public.\"WriteAsPosts\"")
+                .ConfigureAwait(false);
+
+            if (posts is null || !posts.Any())
             {
-                return View(allPosts);
+                var client = new WriteAsClient("https://write.as/");
+                var allPosts = await client.GetAllPosts("acceleratenetworks");
+
+                foreach (var post in allPosts)
+                {
+                    var result = await connection
+                    .ExecuteAsync("INSERT INTO public.\"WriteAsPosts\"( \"Id\", \"Slug\", \"Appearance\", \"Language\", \"Rtl\", \"CreateDate\", \"LastUpdatedDate\", \"Title\", \"Body\", \"Views\") " +
+                    "VALUES (@Id, @Slug, @Appearance, @Language, @Rtl, @CreateDate, @LastUpdatedDate, @Title, @Body, @Views)",
+                    new { post.Id, post.Slug, post.Appearance, post.Language, post.Rtl, post.CreateDate, post.LastUpdatedDate, post.Title, post.Body, post.Views })
+                    .ConfigureAwait(false);
+                }
+
+                posts = await connection
+                    .QueryAsync<WriteAs.NET.Client.Models.Post>("SELECT \"Id\", \"Slug\", \"Appearance\", \"Language\", \"Rtl\", \"CreateDate\", \"LastUpdatedDate\", \"Title\", \"Body\", \"Views\" " +
+                    "FROM public.\"WriteAsPosts\"")
+                    .ConfigureAwait(false);
+            }
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return View(posts);
             }
             else
             {
-                var post = allPosts.FirstOrDefault(x => x.Title.Contains(postName));
-                return post is not null ? View(post) : View(allPosts);
+                var post = posts.Where(x => x.Title.ToLowerInvariant().Contains(query.ToLowerInvariant())).ToList();
+                return post is not null ? View(post) : View(posts);
             }
         }
     }
