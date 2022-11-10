36c0b5d42703dead39ebbaabbf256ec05affc69a
Thomas Ryan <uncheckederror@users.noreply.github.com>
2022-11-09 16:32:09 -0800

    Updated packages and fixed a null reference issue on the new clients form.

--- NumberSearch.Mvc/Controllers/NewClientController.cs
+++ NumberSearch.Mvc/Controllers/NewClientController.cs
@@ -92,41 +92,6 @@ namespace NumberSearch.Mvc.Controllers
                 ProductOrders = productOrders.ToArray(),
                 Products = products.ToArray(),
                 PhoneNumbers = allNumbers ?? Array.Empty<string>(),
-                //NewClient = new NewClient
-                //{
-                //    TextingService = true,
-                //    TextingServiceName = "ZipWhip",
-                //    SpeedDialKeys = new SpeedDialKey[] { new SpeedDialKey { LabelOrName = "Front Desk", NumberOrExtension = "Ext 1" } },
-                //    AfterHoursVoicemail = "Forward all voicemails to the front desk (Ext 1)",
-                //    BusinessHours = "We are open from 8 AM to 5 PM and closed on the weekends.",
-                //    CustomHoldMusic = true,
-                //    HoldMusicDescription = "Please play X gonna give it to ya. Thanks",
-                //    Intercom = true,
-                //    IntercomRegistrations = new IntercomRegistration[] { new IntercomRegistration { ExtensionSendingIntercom = 1, ExtensionRecievingIntercom = 2 } },
-                //    ExtensionRegistrations = new ExtensionRegistration[] {
-                //        new ExtensionRegistration { ExtensionNumber = 103, NameOrLocation = "Jessica", Email = "jessia@exmaple.co", ModelOfPhone = "GXP2170", OutboundCallerId = "2068589310" },
-                //        new ExtensionRegistration { ExtensionNumber = 202, NameOrLocation = "Jared", Email = "jared@example.co", ModelOfPhone = "Linphone", OutboundCallerId = "2068589313" },
-                //        new ExtensionRegistration { ExtensionNumber = 105, NameOrLocation = "Will", Email = "will@example", ModelOfPhone = "GRP2615", OutboundCallerId = "2068588757" },
-                //        new ExtensionRegistration { ExtensionNumber = 404, NameOrLocation = "Loading Dock", ModelOfPhone = "GRP2612W", OutboundCallerId = "2068588757" },
-                //        new ExtensionRegistration { ExtensionNumber = 541, NameOrLocation = "Kitchen", ModelOfPhone = "GRP2612W", OutboundCallerId = "2068588757" }
-                //    },
-                //    PhoneMenu = true,
-                //    PhonesToRingOrMenuDescription = "Please ring to a phone menu that lists our extension numbers and the dials through to the front desk.",
-                //    OverheadPaging = true,
-                //    OverheadPagingDescription = "Big loud sounds come from the front desk and go to the loading dock.",
-                //    PhoneOfflineInstructions = "Forward the call to the front desk.",
-                //    FollowMeRegistrations = new FollowMeRegistration[] {
-                //        new FollowMeRegistration { NumberOrExtension = "1", CellPhoneNumber = "2068588757", UnreachablePhoneNumber = "Front Desk" }
-                //    },
-                //    NumberDescriptions = new NumberDescription[] {
-                //        new NumberDescription { PhoneNumber = "2068589310", Description = "Ring to 103 Jessica" },
-                //        new NumberDescription { PhoneNumber = "2068589313", Description = "Ring to 105 Jared" },
-                //        new NumberDescription { PhoneNumber = "2068588757", Description = "Ring 103 and 105", Prefix = "Support" }
-                //    },
-                //    OrderId = order.OrderId,
-                //    BillingClientId = order.BillingClientId,
-                //    NewClientId = Guid.NewGuid()
-                //}
             };
 
             return View("Index", form);
@@ -136,7 +101,7 @@ namespace NumberSearch.Mvc.Controllers
         [HttpPost("Cart/Order/{orderId}/NewClient/")]
         [ValidateAntiForgeryToken]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public async Task<IActionResult> SubmitNewClientAsync(Guid orderId, NewClient newClient)
+        public async Task<IActionResult> SubmitNewClientAsync(Guid orderId, NewClient? newClient)
         {
             var order = await Order.GetByIdAsync(orderId, _postgresql).ConfigureAwait(false);
 
@@ -144,7 +109,7 @@ namespace NumberSearch.Mvc.Controllers
             {
                 var checkNewClient = await NewClient.GetAsync(newClient.NewClientId, _postgresql).ConfigureAwait(false);
 
-                // If it exist update it, if not then create it.
+                // If it exists update it, if not then create it.
                 if (checkNewClient?.NewClientId == newClient?.NewClientId)
                 {
                     var checkUpdate = await newClient!.PutAsync(_postgresql).ConfigureAwait(false);
@@ -170,6 +135,7 @@ namespace NumberSearch.Mvc.Controllers
                 newClient.IntercomRegistrations = await IntercomRegistration.GetByNewClientAsync(newClient.NewClientId, _postgresql).ConfigureAwait(false);
                 newClient.SpeedDialKeys = await SpeedDialKey.GetByNewClientAsync(newClient.NewClientId, _postgresql).ConfigureAwait(false);
                 newClient.FollowMeRegistrations = await FollowMeRegistration.GetByNewClientAsync(newClient.NewClientId, _postgresql).ConfigureAwait(false);
+                newClient.PhoneMenuOptions = await PhoneMenuOption.GetByNewClientAsync(newClient.NewClientId, _postgresql).ConfigureAwait(false);
 
                 // There is info entered make sure the users sees it.
                 if (newClient.IntercomRegistrations.Any())
