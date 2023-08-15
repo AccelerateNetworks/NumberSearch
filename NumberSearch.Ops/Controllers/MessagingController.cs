4bcf64e9f55429c27f5ede83b0dd347d96dab93b
Thomas Ryan <uncheckederror@users.noreply.github.com>
2023-08-15 13:51:35 -0700

    Yet more work in support of #383.

--- NumberSearch.Ops/Controllers/MessagingController.cs
+++ NumberSearch.Ops/Controllers/MessagingController.cs
@@ -15,6 +15,7 @@ using NumberSearch.Ops.Models;
 
 using System;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace NumberSearch.Ops.Controllers
@@ -86,26 +87,41 @@ namespace NumberSearch.Ops.Controllers
         [Route("/Messaging/Register")]
         public async Task<IActionResult> RegisterAsync(string dialedNumber)
         {
-            string message = string.Empty;
-            string alertType = "alert-success";
+            var result = new MessagingResult { AlertType = "alert-success" };
             bool checkParse = PhoneNumbersNA.PhoneNumber.TryParse(dialedNumber, out var phoneNumber);
             if (checkParse && phoneNumber is not null && !string.IsNullOrWhiteSpace(phoneNumber.DialedNumber))
             {
-                var refresh = await $"{_baseUrl}client?asDialed={phoneNumber.DialedNumber}".WithOAuthBearerToken(_messagingToken).GetJsonAsync<ClientRegistration>();
-                var registrationRequest = new RegistrationRequest
+                try
                 {
-                    DialedNumber = refresh.AsDialed,
-                    CallbackUrl = refresh.CallbackUrl,
-                    ClientSecret = refresh.ClientSecret,
-                };
-                var request = await $"{_baseUrl}client/register".WithOAuthBearerToken(_messagingToken).PostJsonAsync(registrationRequest);
-                var response = await request.GetJsonAsync<RegistrationResponse>();
-                message = $"✔️ Reregistration complete! {response.Message}";
+                    var refresh = await $"{_baseUrl}client?asDialed={phoneNumber.DialedNumber}".WithOAuthBearerToken(_messagingToken).GetJsonAsync<ClientRegistration>();
+                    var registrationRequest = new RegistrationRequest
+                    {
+                        DialedNumber = refresh.AsDialed,
+                        CallbackUrl = refresh.CallbackUrl,
+                        ClientSecret = refresh.ClientSecret,
+                    };
+                    var request = await $"{_baseUrl}client/register".WithOAuthBearerToken(_messagingToken).PostJsonAsync(registrationRequest);
+                    var response = await request.GetJsonAsync<RegistrationResponse>();
+                    result.Message = $"✔️ Reregistration complete! {response.Message}";
+                }
+                catch (FlurlHttpException ex)
+                {
+                    result.RegistrationRequest = new RegistrationRequest { DialedNumber = dialedNumber };
+                    result.Message = $"❓Please register this number for service. {await ex.GetResponseStringAsync()}";
+                    result.AlertType = "alert-warning";
+                } catch (Exception ex)
+                {
+                    result.Message = $"{ex.Message} {ex.StackTrace}";
+                    result.AlertType = "alert-danger";
+                }
             }
             var stats = await $"{_baseUrl}client/usage".WithOAuthBearerToken(_messagingToken).GetJsonAsync<UsageSummary[]>();
             var failures = await $"{_baseUrl}message/all/failed?start={DateTime.Now.AddDays(-3).ToShortDateString()}&end={DateTime.Now.AddDays(1).ToShortDateString()}".WithOAuthBearerToken(_messagingToken).GetJsonAsync<MessageRecord[]>();
             var ownedNumbers = await _context.OwnedPhoneNumbers.ToArrayAsync();
-            return View("Index", new MessagingResult { UsageSummary = stats.OrderByDescending(x => x.OutboundSMSCount).ToArray(), FailedMessages = failures.OrderByDescending(x => x.DateReceivedUTC).ToArray(), Owned = ownedNumbers, Message = message, AlertType = alertType });
+            result.UsageSummary = stats.OrderByDescending(x => x.OutboundSMSCount).ToArray();
+            result.FailedMessages = failures.OrderByDescending(x => x.DateReceivedUTC).ToArray();
+            result.Owned = ownedNumbers;
+            return View("Index", result);
         }
 
         [Authorize]
