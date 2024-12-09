895b51a88bb4560b650547e70dc3ba43feb48510
Thomas Ryan <thomas.ryan@outlook.com>
2024-12-08 20:28:23 -0800

    Enabled the expanded exceptions page in support of #478.

--- NumberSearch.Ops/Startup.cs
+++ NumberSearch.Ops/Startup.cs
@@ -114,6 +114,7 @@ namespace NumberSearch.Ops
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
